5f7348dfbc158ebbfb8a8e0a3663089efc6b1755
darkruby <art@darkruby.co.uk>
2021-10-29 14:19:32 +0100

    parallelism with rayon

--- src/convert.rs
+++ src/convert.rs
@@ -1,8 +1,8 @@
 use anyhow::*;
 use image::png::PngEncoder;
 use image::ColorType;
-use num::abs;
 use num::Complex;
+use rayon::prelude::*;
 use std::fs::File;
 
 type Cpx = Complex<f64>;
@@ -23,36 +23,33 @@ pub fn stability(c: &Cpx) -> u8 {
     255
 }
 
-pub fn generate(width: u32, height: u32) -> (u32, u32, impl Iterator<Item = u8>) {
-    let RIGHT_MOST = width as i32 / 2;
-    let LEFT_MOST = -RIGHT_MOST;
-    let TOP_MOST = height as i32 / 2;
-    let BOTTOM_MOST = -TOP_MOST;
+pub fn generate(width: u32, height: u32) -> (u32, u32, Vec<u8>) {
+    let right_most = width as i32 / 2;
+    let left_most = -right_most;
+    let top_most = height as i32 / 2;
+    let bottom_most = -top_most;
 
-    dbg!(LEFT_MOST, RIGHT_MOST);
-    dbg!(TOP_MOST, BOTTOM_MOST);
-
-    let xs = (LEFT_MOST..RIGHT_MOST).cycle();
-    let ys = (BOTTOM_MOST..TOP_MOST)
+    let xs = (left_most..right_most).cycle();
+    let ys = (bottom_most..top_most)
         .rev()
         .flat_map(move |y| [y].into_iter().cycle().take(width as usize));
-    let xys = xs.zip(ys);
+    let xys = xs.zip(ys).collect::<Vec<(i32, i32)>>();
 
-    let iter = xys.map(move |(x, y)| {
-        let re = (-1.5 * x as f64) / LEFT_MOST as f64;
-        let im = (1.5 * y as f64) / TOP_MOST as f64;
+    let iter = xys.par_iter().map(move |&(x, y)| {
+        let re = (-1.1 * x as f64) / left_most as f64;
+        let im = (1.1 * y as f64) / top_most as f64;
         let cpx = Cpx { re, im };
         stability(&cpx)
     });
 
-    (width, height, iter)
+    (width, height, iter.collect())
 }
 
 pub fn write_image(filename: &str) -> Result<()> {
-    let (width, height, data) = generate(320, 240);
+    let (width, height, data) = generate(3200, 2400);
+    // let (width, height, data) = generate(320, 240);
     let file = File::create(filename)?;
 
-    let data: Vec<u8> = data.collect();
     PngEncoder::new(file).encode(&data, width, height, ColorType::L8)?;
     Ok(())
 }
