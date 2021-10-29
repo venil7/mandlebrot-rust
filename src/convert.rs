b9b6664110d6bf50b2f06e13da56e02cefd5bb18
darkruby <art@darkruby.co.uk>
2021-10-29 13:54:49 +0100

    working mandelbrot

--- src/convert.rs
+++ src/convert.rs
@@ -1,198 +1,58 @@
-use image::jpeg::JPEGDecoder;
-use image::png::PNGEncoder;
+use anyhow::*;
+use image::png::PngEncoder;
 use image::ColorType;
-use image::ImageDecoder;
-use image::ImageResult;
+use num::abs;
 use num::Complex;
 use std::fs::File;
 
-pub type Pair<T> = (T, T);
-pub type Triplet<T> = (T, T, T);
-pub type RGB = Triplet<u8>;
-pub type Coord = Pair<usize>;
-pub type Pixel = (Coord, RGB);
-pub type ComplexCoord = Pair<Complex<f64>>;
-pub type ComplexPixel = (Complex<f64>, RGB);
+type Cpx = Complex<f64>;
 
-pub struct Image {
-  pub bounds: Coord,
-  pub pixels: Vec<Pixel>,
+pub fn is_local(Cpx { re, im }: Cpx) -> bool {
+    // (x - center_x)² + (y - center_y)² < radius².
+    (re * re) + (im * im) <= 1.
 }
 
-pub fn pixel_to_complex(
-  (x, y): &Coord,
-  (width, height): &Coord,
-  (complex_top_left, complex_bottom_right): &ComplexCoord,
-) -> Complex<f64> {
-  let re_width = complex_bottom_right.re - complex_top_left.re;
-  let im_height = complex_top_left.im - complex_bottom_right.im;
-  Complex {
-    re: complex_top_left.re + re_width * (*x as f64 / *width as f64),
-    im: complex_top_left.im - im_height * (*y as f64 / *height as f64),
-  }
-}
-
-pub fn complex_to_pixel(
-  Complex { re, im }: &Complex<f64>,
-  (width, height): &Coord,
-  (complex_top_left, complex_bottom_right): &ComplexCoord,
-) -> Coord {
-  let re_width = complex_bottom_right.re - complex_top_left.re;
-  let im_height = complex_top_left.im - complex_bottom_right.im;
-  (
-    (*width as f64 * (re - complex_top_left.re) / re_width) as usize,
-    (*height as f64 * (im - complex_bottom_right.im) / im_height) as usize,
-  )
-}
-
-pub fn complex_bounds((width, height): &Coord) -> ComplexCoord {
-  (
-    Complex {
-      re: *width as f64 / -2.0,
-      im: *height as f64 / -2.0,
-    },
-    Complex {
-      re: *width as f64 / 2.0,
-      im: *height as f64 / 2.0,
-    },
-  )
-}
-
-pub fn read_image(filename: &str) -> ImageResult<Image> {
-  let input = File::open(filename)?;
-  let decoder = JPEGDecoder::new(input)?;
-  let (width, height) = decoder.dimensions();
-  let (width, height) = (width as usize, height as usize);
-
-  Ok(Image {
-    bounds: (width, height),
-    pixels: decoder
-      .read_image()?
-      .as_slice()
-      .chunks(3)
-      .enumerate()
-      .map(|(i, rgb)| ((i % width, i / width), (rgb[0], rgb[1], rgb[2])))
-      .collect(),
-  })
-}
-
-pub fn write_image(
-  filename: &str,
-  Image {
-    pixels,
-    bounds: (width, height),
-    ..
-  }: &Image,
-) -> Result<(), std::io::Error> {
-  let output = File::create(filename)?;
-  let encoder = PNGEncoder::new(output);
-  let pixels: Vec<u8> = pixels
-    .iter()
-    .flat_map(|(_, (r, g, b))| vec![*r, *g, *b])
-    .collect();
-  encoder.encode(&pixels, *width as u32, *height as u32, ColorType::RGB(8))
-}
-
-pub fn min(a: f64, b: f64) -> f64 {
-  if a < b {
-    a
-  } else {
-    b
-  }
-}
-
-pub fn max(a: f64, b: f64) -> f64 {
-  if a > b {
-    a
-  } else {
-    b
-  }
-}
-
-pub fn calc_complex_bounds(complex_pixels: &[ComplexPixel]) -> ComplexCoord {
-  let init: ComplexCoord = (Complex { re: 0.0, im: 0.0 }, Complex { re: 0.0, im: 0.0 });
-  complex_pixels
-    .iter()
-    .map(|(c, _)| c)
-    .fold(init, |(top_left, bottom_right), item| {
-      (
-        Complex {
-          re: min(top_left.re, item.re),
-          im: max(top_left.im, item.im),
-        },
-        Complex {
-          re: max(bottom_right.re, item.re),
-          im: min(bottom_right.im, item.im),
-        },
-      )
-    })
-}
-
-pub fn calc_bounds(pixels: &[Pixel]) -> Coord {
-  let init = (0, 0);
-  pixels
-    .iter()
-    .map(|(c, _)| c)
-    .fold(init, |(width, height), item| {
-      (std::cmp::max(item.0, width), std::cmp::max(item.1, height))
-    })
-}
-
-pub fn multiply(complex_pixels: &[ComplexPixel]) -> Vec<ComplexPixel> {
-  complex_pixels
-    .iter()
-    .map(|(c, rgb)| (c * Complex { re: 1.0, im: 0.1 }, *rgb))
-    .collect()
-}
-
-pub fn compare((p1, _): &Pixel, (p2, _): &Pixel, width: usize) -> std::cmp::Ordering {
-  (p1.1 * width + p1.0).cmp(&(p2.1 * width + p2.0))
-}
-
-pub fn transform(Image { bounds, pixels }: &Image) -> Image {
-  let complex_pixels: Vec<ComplexPixel> = multiply(&to_complex_pixels(pixels, bounds));
-  let complex_bounds: ComplexCoord = calc_complex_bounds(&complex_pixels);
-  let mut pixels: Vec<Pixel> = complex_pixels
-    .iter()
-    .map(|(c, rgb)| (complex_to_pixel(c, &bounds, &complex_bounds), *rgb))
-    .collect();
-  pixels.sort_by(|p1, p2| compare(p1, p2, bounds.0));
-  let bounds = calc_bounds(&pixels);
-  Image { bounds, pixels }
-}
-
-pub fn to_complex_pixels(pixels: &[Pixel], bounds: &Coord) -> Vec<ComplexPixel> {
-  pixels
-    .iter()
-    .map(|(coord, rgb)| {
-      (
-        pixel_to_complex(coord, bounds, &complex_bounds(bounds)),
-        *rgb,
-      )
-    })
-    .collect()
-}
-
-#[test]
-fn test_1() {
-  let bounds: Coord = (4000, 3000);
-  let cbounds = complex_bounds(&bounds);
-  let pixel = (0, 0);
-  let result = pixel_to_complex(&pixel, &bounds, &cbounds);
-  assert_eq!(
-    result,
-    Complex {
-      re: -2000.0,
-      im: -1500.0
+pub fn stability(c: &Cpx) -> u8 {
+    let mut n = Cpx::default();
+    for i in 0..=255 {
+        n = (n * n) + c;
+        if !is_local(n) {
+            return i;
+        };
     }
-  );
+    255
 }
-#[test]
-fn test_2() {
-  let pixel = (0, 0);
-  let bounds: Coord = (4000, 3000);
-  let cbounds = complex_bounds(&bounds);
-  let complex = pixel_to_complex(&pixel, &bounds, &cbounds);
-  let result = complex_to_pixel(&complex, &bounds, &cbounds);
-  assert_eq!(result, pixel);
+
+pub fn generate(width: u32, height: u32) -> (u32, u32, impl Iterator<Item = u8>) {
+    let RIGHT_MOST = width as i32 / 2;
+    let LEFT_MOST = -RIGHT_MOST;
+    let TOP_MOST = height as i32 / 2;
+    let BOTTOM_MOST = -TOP_MOST;
+
+    dbg!(LEFT_MOST, RIGHT_MOST);
+    dbg!(TOP_MOST, BOTTOM_MOST);
+
+    let xs = (LEFT_MOST..RIGHT_MOST).cycle();
+    let ys = (BOTTOM_MOST..TOP_MOST)
+        .rev()
+        .flat_map(move |y| [y].into_iter().cycle().take(width as usize));
+    let xys = xs.zip(ys);
+
+    let iter = xys.map(move |(x, y)| {
+        let re = (-1.5 * x as f64) / LEFT_MOST as f64;
+        let im = (1.5 * y as f64) / TOP_MOST as f64;
+        let cpx = Cpx { re, im };
+        stability(&cpx)
+    });
+
+    (width, height, iter)
+}
+
+pub fn write_image(filename: &str) -> Result<()> {
+    let (width, height, data) = generate(320, 240);
+    let file = File::create(filename)?;
+
+    let data: Vec<u8> = data.collect();
+    PngEncoder::new(file).encode(&data, width, height, ColorType::L8)?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use mandelbrot::convert::*;
 
-fn main() {
-  let args: Vec<String> = std::env::args().collect();
-  match args.as_slice() {
-    [_, path_in, path_out] => {
-      match read_image(path_in).map(|image| write_image(path_out, &transform(&image))) {
-        Ok(_) => println!("done"),
-        Err(err) => println!("err {}", err),
-      }
-    }
-    _ => print!("not enough args"),
-  }
+fn main() -> anyhow::Result<()> {
+    write_image("./mandlebrot.png")?;
+    Ok(())
 }
